22f0989f34ac80027debce36ae64406716ccb895
Alistair Curd <fbsacu@leeds.ac.uk>
2024-11-26 14:09:59 +0000

    Correct image loading; improve output location and handling.

--- abovethreshold.py
+++ abovethreshold.py
@@ -10,24 +10,35 @@ University of Leeds
 
 import argparse
 import re
+import sys
 from pathlib import Path
 
 import numpy as np
 from skimage.io import imread, imsave
 
 
-def get_inputs():
-    """ Get CLI inputs."""
+def get_inputs(default_out_dir_arg):
+    """ Get CLI inputs.
+    
+    Args:
+        default_out_dir_arg (str):
+            String to diaply for default output directory path.
+
+    """
 
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    ### ADD DESCRIPTION
+    ### REQUIRES FILENAMES TO END IN <integer>.tiff
+
     parser.add_argument('-i', '--input-dir',
                         type=str,
                         default='.',
                         help='Path to input directory.')
     parser.add_argument('-o', '--output-dir',
                         type=str,
-                        default='./events',
+                        default=default_out_dir_arg,
                         help='Path to output directory.')
     parser.add_argument('-t', '--threshold',
                         type=float,
@@ -67,41 +78,79 @@ def sortimagenames_by_num(filename_list):
 
     # Get integers at end of filename stems
     for filename in filename_list:
-        imagenumber = re.search(r'(\d+)\.', filename).group()
-        imagenumber = re.search(r'(\d+)', imagenumber).group()
-        filename_num_dict[filename] = int(imagenumber)
+        # print(filename)
+        try:
+            imagenumber = re.search(r'\d+$', filename).group()
+            filename_num_dict[filename] = int(imagenumber)
+            # print(imagenumber)
+        except AttributeError:
+            print(f'AttributeError. Could not extract number from {filename}')
+            # sys.exit()
 
     # Sorted filename list by image number
     filename_list = sorted(filename_num_dict, key=filename_num_dict.get)
+    print(f'{len(filename_list)} image files found.')
 
     return filename_num_dict, filename_list
 
 
 def main():
     """Extract chosen sequences and save."""
-    user_inputs = get_inputs()
+    # Get and display inputs
+    default_out_dir_arg = '../<input-dir>_events_<params>'
+    user_inputs = get_inputs(default_out_dir_arg)
+
     input_dir = Path(user_inputs.input_dir)
     output_dir = user_inputs.output_dir
-    if output_dir == './events':
-        output_dir = input_dir / 'events'
-    else:
-        output_dir = Path(output_dir)
     threshold = user_inputs.threshold
     before = user_inputs.before
     after = user_inputs.after
+
+    if output_dir == default_out_dir_arg:
+        output_dir = input_dir.parent / (
+            input_dir.name + f'_events_th{threshold}_b{before}_a{after}')
+    else:
+        output_dir = Path(output_dir)
+
+    try:
+        output_dir.mkdir(parents=True, exist_ok=False)
+    except FileExistsError:
+        print('\nThe output directory already exists'
+              ' and you may be overwriting or confusing data:'
+              f'\n({output_dir})')
+        continue_decision = None
+        while continue_decision != 'y' and continue_decision != 'n':
+            continue_decision = input('\nDo you want to continue? y/n: ')
+            if continue_decision == 'y':
+                pass
+            if continue_decision == 'n':
+                print('\nOk, exiting...')
+                sys.exit()
+
+    print('')
+    print(f'Using files from {input_dir}.')
+    print(f'Saving extracted sequences in {output_dir}.')
+    print('')
 
     # Get sorted filenames, no extension
     filename_list = []
     for child in input_dir.iterdir():
         filename_list.append(child.stem)
+        # print(child.stem)
     filename_num_dict, filename_list = sortimagenames_by_num(filename_list)
 
-    # Load sorted images into image stack
-    input_seq = np.zeros(10, 50, 6000)
-    for i, in enumerate(input_seq):
-        input_seq[i] = imread(input_dir / filename_list[i] + 'tiff')
+    if len(filename_list) == 0:
+        print('No valid image files.')
+        print('Exiting...')
+        sys.exit()
 
-    # Load sorted images into image stack
+    ## Load sorted images into image stack
+    # First get image dimensions
+    image0 = imread(input_dir / (filename_list[0] + '.tiff'))
+    print(f'Single-image dimensions are: {image0.shape}')
+    input_seq = np.zeros((image0.shape[0], image0.shape[1], len(filename_list)))
+    for i in range(input_seq.shape[2]):
+        input_seq[:,:,i] = imread(input_dir / (filename_list[i] + '.tiff'))
 
     # Find and save desired sequences
     i = 0
